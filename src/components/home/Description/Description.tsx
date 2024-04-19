dca0140d1ffa1b1a59da485a56283d58fc1781c7
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-19 17:16:30 -0600

    Como agregar archivos estáticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,9 @@
 export const Description = () => {
     return (
         <section>
-            <h1>Descripción</h1>
+            <img src="/images/description.jpeg" alt="products marketplace" />
+            <h2>Description</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta illum quasi fuga eveniet quam ducimus perspiciatis eos cum inventore accusantium? Libero consequuntur sunt tempore est! Placeat molestias ea debitis incidunt?</p>
         </section>
     )
 }
