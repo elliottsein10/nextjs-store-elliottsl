ed742b5c697f50f28932a2b09a4e429d5eca2045
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-19 23:14:32 -0600

    Cuanto utilizar layout o template

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,8 @@ import { MainProducts } from "app/components/home/MainProducts";
 export default function Home() {
   return (
     <main>
-      <MainProducts/>
+      <h1>Products</h1>
+      {/* <MainProducts/> */}
     </main>
   );
 }
--- src/app/(home)/template.tsx
+++ src/app/(home)/template.tsx
@@ -1,7 +1,14 @@
+"use client"
 import { Hero } from "app/components/home/Hero"
 import { Description } from "app/components/home/Description"
+import { useEffect } from "react"
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
+    
+    useEffect(() => {
+        console.log('envío de métricas')
+    }, [])
+
     return (
         <div>
             <Hero />
