50bbe54e1439f2792d1730fa2ec994efdee30c3a
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-20 02:29:46 -0600

    Manejando SEO de una página

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,13 @@
 import { Description } from "app/components/home/Description";
 import { Hero } from "app/components/home/Hero";
 import { MainProducts } from "app/components/home/MainProducts";
+import {Metadata} from "next"
+
+export const metadata: Metadata = {
+  title: "Future world",
+  description: "Welcome to the future world, an eccomerce from other cent",
+  keywords: ["ecomerce", "future", "world", "technology"] 
+}
 
 export default function Home() {
   return (
