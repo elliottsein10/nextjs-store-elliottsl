363c3904379a14a80d3cd5b3b2d6fe3bf5ddd092
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-20 01:23:03 -0600

    Proyecto - Página de Producto y arreglos de sitio

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,10 +1,16 @@
-"use client"
+import { ProductView } from "app/components/product/ProductView"
+import { getProducts } from "app/services/shopify/products"
 
-import { useParams, useSearchParams } from "next/navigation"
 
-export default function ProductPage(){
-    const params = useParams()
-    const searchParams = useSearchParams()
-    const id = searchParams.get("id")
-    return <h1>Product page</h1>
+interface ProductPageProps {
+    searchParams: {
+        id: string
+    }
+}
+
+export default async function ProductPage({ searchParams }: ProductPageProps){
+    const id = searchParams.id
+    const products = await getProducts(id)
+    const product = products[0]
+    return <ProductView product={product}/>
 }
