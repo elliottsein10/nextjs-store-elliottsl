f12e71177bc3626356860b68f377f7c312f1c7d5
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-19 19:45:40 -0600

    Como obtener información de una API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,6 +1,17 @@
 "use client"
 
-export const MainProducts = () => {
+const getProducts = async () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-04/products.json`, {
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
+        })
+    })
+    const data = await response.json()
+    return data
+}
+
+export const MainProducts = async () => {
+    const products = await getProducts()
     return (
         <section>
             <h1>MainProducts</h1>
