2d99c342f9ab42662cdcc06170ed1636cb876568
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-19 23:36:17 -0600

    Arquitectura profesional para data fetching

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,8 +5,7 @@ import { MainProducts } from "app/components/home/MainProducts";
 export default function Home() {
   return (
     <main>
-      <h1>Products</h1>
-      {/* <MainProducts/> */}
+      <MainProducts/>
     </main>
   );
 }
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -5,21 +5,12 @@ export const Header = () => {
         <header>
           <nav>
             <ul>
-              <li>
               <Link href="/">
                 <li>Home</li>
               </Link>
-              </li>
-              <li>
               <Link href="/store">
                 <li>Store</li>
               </Link>
-              </li>
-              <li>
-              <Link href="/test">
-                <li>Test</li>
-              </Link>
-              </li>
             </ul>
           </nav>
         </header>
